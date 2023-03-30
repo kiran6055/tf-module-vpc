97317ced092a6a6ee44c2cb2b97fc855c6dffeb9
kiran1616 <kirankumar7163@gmail.com>
2023-03-30 13:08:43 +0530

    updated script

--- main.tf
+++ main.tf
@@ -171,6 +171,13 @@ resource "aws_security_group" "allow_tls" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    description = "http from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port        = 0
