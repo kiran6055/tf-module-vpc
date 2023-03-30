65514588a836bcedb223d321f86ee6236059abcc
kiran1616 <kirankumar7163@gmail.com>
2023-03-30 12:32:42 +0530

    updated script

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ resource "aws_subnet" "public" {
   count      = length(var.public_subnets_cidr)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnets_cidr[count.index]
+  availability_zone = var.availability_zones[count.index]
 
   tags       = merge(
     local.common_tags,
@@ -24,6 +25,7 @@ resource "aws_subnet" "private" {
   count      = length(var.private_subnets_cidr)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.private_subnets_cidr[count.index]
+  availability_zone = var.availability_zones[count.index]
 
   tags       = merge(
     local.common_tags,
@@ -138,7 +140,7 @@ resource "aws_route" "peer" {
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 }
 
-// create EC2 instance
+create EC2 instance
 
 data "aws_ami" "centos8" {
   most_recent = true
