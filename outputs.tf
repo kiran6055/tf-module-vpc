34e7a2381aead86f2dc0fce3ab95989c897c122a
kiran1616 <kirankumar7163@gmail.com>
2023-04-04 18:47:32 +0530

    updated script

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,7 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 
-output "vpc_peering_connection_id" {
 
-  value = "aws_vpc_peering_connection.peer.id"
+output "vpc_peering_connection_id" {
+  value = aws_vpc_peering_connection.peer.id
 }
