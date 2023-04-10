92e3a60c1e0e1ce0658695cb1cf2af9a74bead72
kiran1616 <kirankumar7163@gmail.com>
2023-04-10 16:17:57 +0530

    updated script

--- subnets/main.tf
+++ subnets/main.tf
@@ -34,7 +34,7 @@ resource "aws_route_table_association" "association" {
 
 
 resource "aws_route" "internet_gw_route" {
-  count                       = var.internet_gw == null ? 1 : 0
+  count                       = var.internet_gw ? 1 : 0
   route_table_id              = aws_route_table.route_table.id
   destination_cidr_block      = "0.0.0.0/0"
   gateway_id                  = var.gateway_id
